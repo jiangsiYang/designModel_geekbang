003f6f3dba2ecab619acc8d6c450b916a8a6ded9
jiangsiYang <353098889@qq.com>
2020-02-17 23:33:10 +0800

    工厂方法

--- src/main/java/com/design/u044/F/RuleConfigSourceF.java
+++ src/main/java/com/design/u044/F/RuleConfigSourceF.java
@@ -7,6 +7,7 @@ import com.design.u044.RuleConfig;
  * 利用工厂方法模式的版本。
  * 当我们新增一种 parser 的时候，只需要新增一个实现了 IRuleConfigParserFactory 接口的 Factory 类即可。所以，工厂方法模式比起简单工厂模式更加符合开闭原则。
  * 但是判断用哪一种工厂，还是要加if else 的判断啊。。
+ * 可以为工厂类再创建一个简单工厂，也就是工厂的工厂，用来创建工厂类对象。
  */
 public class RuleConfigSourceF {
 
