36b155096fc8fc7ec14b34560af0180f20d07740
jiangsiYang <353098889@qq.com>
2020-03-14 12:24:22 +0800

    test

--- src/main/java/com/design/u055/StringTest.java
+++ src/main/java/com/design/u055/StringTest.java
@@ -7,8 +7,20 @@ public class StringTest {
         String c = "ab";
         String d = new String("ab");
 
+
+        String e1 = "dc";
+        String e2 = new String("dc");
+        e2 = e2.intern();   //如果只是e2.intern(),没有重新赋值给e2,那么还会是false
+
+        String f = new String("ef");
+        f = f.intern();
+        String g = "ef";
+
+
         System.out.println(a == b);
         System.out.println(a == c);
         System.out.println(a == d);
+        System.out.println(e1 == e2);
+        System.out.println(f == g);
     }
 }
